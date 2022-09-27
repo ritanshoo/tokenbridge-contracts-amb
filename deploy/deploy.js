bde54359ac4cfba3dc5844d4cfd5e842a7ea1115
ritanshoo <ritanshukumarsingh8922@gmail.com>
2022-09-27 06:32:35 +0530

    upto initialize part

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -57,6 +57,49 @@ async function deployAMBErcToErc() {
   const deployForeign = require('./src/amb_erc677_to_erc677/foreign')
   const initialize = require('./src/amb_erc677_to_erc677/initialize');
   console.log("1 ====================")
+  // await preDeploy()
+  console.log("2 ====================")
+
+  // const { homeBridgeMediator, bridgeableErc677 } = await deployHome()
+  console.log("3 deployHome ke baad  ====================")
+
+  // const { foreignBridgeMediator } = await deployForeign()
+  console.log("4 deployForeign ke baad  ====================")
+
+  const homeBridge = "0xed47976103eBcCF7685e8aF185deD9EcF57E146A"
+  const foreignBridge = "0xA8Ab482F05924A52D3d684240942B57817Cc176A"
+  const homeErc677 = "0x16CE67c0ab3607BD65bd351C0b913E8e8404878A"
+
+  await initialize({
+    homeBridge,
+    foreignBridge,
+    homeErc677
+  })
+
+  console.log("5 initialize ke baad  ====================")
+  
+  console.log('\nDeployment has been completed.\n\n')
+  console.log(`[   Home  ] Bridge Mediator: ${homeBridge}`)
+  console.log(`[   Home  ] ERC677 Bridgeable Token: ${homeErc677}`)
+  console.log(`[ Foreign ] Bridge Mediator: ${foreignBridge}`)
+  console.log(`[ Foreign ] ERC677 Token: ${ERC20_TOKEN_ADDRESS}`)
+  // writeDeploymentResults({
+  //   homeBridge: {
+  //     homeBridgeMediator,
+  //     bridgeableErc677
+  //   },
+  //   foreignBridge: {
+  //     foreignBridgeMediator
+  //   }
+  // })
+}
+
+async function deployAMBErcToErcOld() {
+  const preDeploy = require('./src/amb_erc677_to_erc677/preDeploy')
+  const deployHome = require('./src/amb_erc677_to_erc677/home')
+  const deployForeign = require('./src/amb_erc677_to_erc677/foreign')
+  const initialize = require('./src/amb_erc677_to_erc677/initialize');
+  console.log("1 ====================")
   await preDeploy()
   console.log("2 ====================")
 
